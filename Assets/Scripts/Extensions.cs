ae8a9d5bfe81c5bfafd483f55b6b9f89529d6854
thegreatpl <paulmarkwhite@hotmail.co.uk>
2021-01-30 00:48:26 +0000

    Added clothes to player.

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine; 
+
+
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Returns a random value from this collection. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ts"></param>
+        /// <returns></returns>
+        public static T GetRandom<T>(this IEnumerable<T> ts)
+        {
+           return ts.ElementAt(UnityEngine.Random.Range(0, ts.Count()));   
+        }
+    }
+
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public Camera MainCamera; 
+
     public List<PrefabObj> Prefabs;
 
 
@@ -18,21 +21,61 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         World = FindObjectOfType<WorldScript>();
+        MainCamera = Camera.main;
 
-        var entity = Prefabs.FirstOrDefault(x => x.Name == "Entity");
-        var player = Instantiate(entity.Prefab);
-        player.transform.position = World.SpawnPoint;
-        player.AddComponent<PlayerController>();
-
-        var layerprefab = Prefabs.FirstOrDefault(x => x.Name == "AnimationLayer");
-        var layer = Instantiate(layerprefab.Prefab, player.transform);
-        layer.GetComponent<AnimationLayer>().ApplyAnimations(animationLayers[0]); 
-
+        StartCoroutine(LoadScene()); 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    IEnumerator LoadScene()
+    {
+        var entity = Prefabs.FirstOrDefault(x => x.Name == "Entity");
+
+        GameObject player;
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player == null)
+        {
+
+            player = Instantiate(entity.Prefab);
+            player.tag = "Player";
+            player.AddComponent<PlayerController>();
+
+            MainCamera.transform.parent = player.transform;
+
+            yield return null;
+
+            var layerprefab = Prefabs.FirstOrDefault(x => x.Name == "AnimationLayer");
+
+
+
+            var body = Instantiate(layerprefab.Prefab, player.transform);            
+            var legs = Instantiate(layerprefab.Prefab, player.transform);
+            var hair = Instantiate(layerprefab.Prefab, player.transform);
+            var chest = Instantiate(layerprefab.Prefab, player.transform);
+
+            yield return null;
+            var bodyal = body.GetComponent<AnimationLayer>();
+            bodyal.ApplyAnimations(animationLayers.Where(x => x.layer == "Body").GetRandom());
+            bodyal.Sprite.sortingOrder = 0;
+            var hairyal = hair.GetComponent<AnimationLayer>();
+            hairyal.ApplyAnimations(animationLayers.Where(x => x.layer == "Hair").GetRandom());
+            hairyal.Sprite.sortingOrder = 1;
+            var chestyal = chest.GetComponent<AnimationLayer>(); 
+            chestyal.ApplyAnimations(animationLayers.Where(x => x.layer == "Chest").GetRandom());
+            chestyal.Sprite.sortingOrder = 1;
+            var legsyal = legs.GetComponent<AnimationLayer>(); 
+            legsyal.ApplyAnimations(animationLayers.Where(x => x.layer == "Legs").GetRandom());
+            legsyal.Sprite.sortingOrder = 1;  
+
+            DontDestroyOnLoad(player); 
+        }
+        player.transform.position = World.SpawnPoint;
     }
 }
