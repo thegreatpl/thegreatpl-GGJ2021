2a84841882b435eb19ac4d2edecb23adc338933d
thegreatpl <paulmarkwhite@hotmail.co.uk>
2021-01-31 01:58:52 +0000

    Sound effects!

--- Assets/Scripts/Sounds/SoundEffectPlayerScript.cs
+++ Assets/Scripts/Sounds/SoundEffectPlayerScript.cs
@@ -1,9 +1,14 @@
+using Assets.Scripts.Objects;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SoundEffectPlayerScript : MonoBehaviour
 {
+
+    public List<MusicObj> SoundEffects = new List<MusicObj>(); 
+
     /// <summary>
     /// The audio source that will play the effects. 
     /// </summary>
@@ -29,5 +34,21 @@ public class SoundEffectPlayerScript : MonoBehaviour
     {
         AudioSource.clip = clip;
         AudioSource.Play(); 
+    }
+
+
+    public void PlayAudio(string name)
+    {
+        var clip = SoundEffects.FirstOrDefault(x => x.Name == name);
+        if (clip != null)
+            PlayAudio(clip.AudioClip); 
+    }
+
+
+    public void PlayAudioViaTag(string tag)
+    {
+        var clip = SoundEffects.Where(x => x.Tags.Contains(tag)).GetRandom();
+        if (clip != null)
+            PlayAudio(clip.AudioClip); 
     }
 }
